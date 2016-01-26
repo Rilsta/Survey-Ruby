c97bf9b0c3dceeeff46a43daaefe29edf66e2456
Mike Mahoney and Riley Starnes <rstarnes9@gmail.com>
2016-01-26 15:18:30 -0800

    add everything

---
+++ app.rb
@@ -0,0 +1,71 @@
+require('sinatra')
+require('sinatra/reloader')
+require('sinatra/activerecord')
+require('./lib/survey')
+require('./lib/question')
+require('pg')
+require('pry')
+also_reload('./lib/**/*.rb')
+
+get('/') do
+  @surveys = Survey.all
+  erb(:index)
+end
+
+get('/surveys/new') do
+  erb(:survey_form)
+end
+
+post('/surveys') do
+  Survey.create(params)
+  redirect ('/')
+end
+
+get('/surveys/:id/edit') do
+  @survey = Survey.find(params.fetch("id").to_i)
+  erb(:survey_update)
+end
+
+patch('/surveys/:id') do
+  @survey = Survey.find(params.fetch("id").to_i)
+  @survey.update({:title => params.fetch("title")})
+  redirect ('/')
+end
+
+delete('/surveys/:id') do
+  @survey = Survey.find(params.fetch("id").to_i)
+  @survey.destroy
+  redirect ('/')
+end
+
+get('/surveys/:id/questions') do
+  @survey = Survey.find(params.fetch("id").to_i)
+  erb(:questions)
+end
+
+get('/surveys/:id/questions/new') do
+  @survey = Survey.find(params.fetch("id").to_i)
+  erb(:question_form)
+end
+
+post('/surveys/:survey_id/questions') do
+  Question.create(:survey_id => params.fetch("survey_id").to_i, :question => params.fetch("question"))
+  redirect ("/surveys/#{params.fetch('survey_id').to_i}/questions")
+end
+
+get('/surveys/:survey_id/questions/:id/edit') do
+  @question = Question.find(params.fetch("id").to_i)
+  erb(:question_update)
+end
+
+patch('/surveys/:survey_id/questions/:id') do
+  @question = Question.find(params.fetch("id").to_i)
+  @question.update(:survey_id => params.fetch("survey_id").to_i, :question => params.fetch("question"))
+  redirect ("/surveys/#{params.fetch('survey_id').to_i}/questions")
+end
+
+delete('/surveys/:survey_id/questions/:id') do
+  @question = Question.find(params.fetch("id").to_i)
+  @question.destroy
+  redirect ("/surveys/#{params.fetch('survey_id').to_i}/questions")
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160126170242) do
+ActiveRecord::Schema.define(version: 20160126192419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160126170242) do
     t.string   "question"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "survey_id"
   end
 
   create_table "surveys", force: :cascade do |t|
