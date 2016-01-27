00941bbc3b50cbfff2efa9a278cbb1582086a3e5
Mike Mahoney and Riley Starnes <rstarnes9@gmail.com>
2016-01-26 16:04:37 -0800

    add integrations tests

--- lib/survey.rb
+++ lib/survey.rb
@@ -1,3 +1,10 @@
 class Survey < ActiveRecord::Base
   has_many :questions
+  before_save(:capitalized)
+
+  private
+
+  def capitalized
+    self.title = self.title.titleize
+  end
 end
--- spec/survey_integration_spec.rb
+++ spec/survey_integration_spec.rb
@@ -1,1 +1,24 @@
 ENV['RACK_ENV'] = 'test'
+
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('survey pathways', {:type => :feature}) do
+  it('adds surveys to homepage') do
+    visit('/')
+    click_link('Add Survey')
+    fill_in('title', with: 'Survey test')
+    click_button('Submit')
+    expect(page).to have_content('Survey Test')
+  end
+  it('add questions to surveys') do
+    survey = create_survey
+    visit("/surveys/#{survey.id}/questions")
+    click_link('Add Questions')
+    fill_in('question', with: 'question test')
+    click_button('Submit')
+    expect(page).to have_content('question test')
+  end
+end
